1039c0625c6be80d481a3769406f8111437efcd9
Francisco Erlyson Pamplona <77758434+DreamEater360@users.noreply.github.com>
2024-02-05 11:22:05 -0300

    Update DriveDefaut.java

--- FRCchassi/src/main/java/frc/robot/commands/DriveDefaut.java
+++ FRCchassi/src/main/java/frc/robot/commands/DriveDefaut.java
@@ -2,12 +2,8 @@ package frc.robot.commands;
 
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.DriveTrainSubsystem;
-//import frc.robot.subsystems.ExampleSubsystem;
-//import edu.wpi.first.networktables.NetworkTable.SubTableListener;
 import edu.wpi.first.wpilibj2.command.Command;
-//import edu.wpi.first.wpilibj2.command.Subsystem;
 
-/** An example command that uses an example subsystem. */
 public class DriveDefaut extends Command {
     private final DriveTrainSubsystem m_drive;
 
@@ -15,25 +11,21 @@ public class DriveDefaut extends Command {
     this.m_drive = m_drive;
     addRequirements(m_drive);
   }
-
-  // Called when the command is initially scheduled.
+  
   @Override
   public void initialize() {
     RobotContainer.driveTrain.stop();
   }
 
-  // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     m_drive.driveWithJoystick(RobotContainer.m_driverController, DriveTrainSubsystem.calc(RobotContainer.m_driverController, 0.7, RobotContainer.m_driverController.getLeftTriggerAxis(), 
     RobotContainer.m_driverController.getRightTriggerAxis()));
   }
 
-  // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {}
 
-  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     return false;
