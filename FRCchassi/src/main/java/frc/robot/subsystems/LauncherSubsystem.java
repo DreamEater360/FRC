06b2bf0172b68b8e09fb0061a6ec6b0a85ff933c
DreamEater <erlysonpamplona1234@gmail.com>
2024-02-24 09:57:38 -0300

    FAÇAM_O_AUTONOMO_AQ

--- FRCchassi/src/main/java/frc/robot/subsystems/LauncherSubsystem.java
+++ FRCchassi/src/main/java/frc/robot/subsystems/LauncherSubsystem.java
@@ -4,23 +4,30 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.Victor;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.OperatorConstants;
 
 public class LauncherSubsystem extends SubsystemBase{
     
-    CANSparkMax launcherLeft = new CANSparkMax(OperatorConstants.launcherLeft, MotorType.kBrushed);
+    CANSparkMax launcherTop = new CANSparkMax(OperatorConstants.launcherLeft, MotorType.kBrushed);
     CANSparkMax launcherRight = new CANSparkMax(OperatorConstants.launcherRight, MotorType.kBrushed);
+    Victor  launcherLeft = new Victor(11);
+    
 
     DifferentialDrive launcher = new DifferentialDrive(launcherLeft, launcherRight);
 
     double launcherSpeed;
+    double curto;
 
     //Calcula a velocidade que o robô tem
-    public static double calcLauncher(XboxController m_secundPiloController, double launcherSpeed){
-        double ttspeed = (launcherSpeed/3);
+    public static double calcLauncher(XboxController m_secundPiloController, double launcherSpeed, double curto){
+        double ttspeed = (launcherSpeed/(3*(curto+0.1)));
         return ttspeed; 
-    
+    }
+
+    public LauncherSubsystem(){
+        launcherTop.follow(launcherRight);
     }
     //Pega os valores obtidos no joystick e calcula o movimento do robô
     public void launcherWithJoystick(XboxController m_secundPilotController, double launcherSpeed){
